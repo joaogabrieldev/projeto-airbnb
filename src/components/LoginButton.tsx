ddf772c01a2588e4f6bb7332e2bc1024b81c1b89
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-09 17:51:21 -0300

    chore: I updated LoginButton props interface name, for IButtonProps, to ILoginButtonProps

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,13 +3,10 @@ import Link from "next/link";
 
 import { defaultUseColor } from "@/assets/defaultVariables";
 
-interface IButtonProps {
-  href: string;
-  value: string;
-  defaultTransition: string;
-}
+import { ILoginButtonProps } from "./components.types";
 
-const LoginButton = ({ value, defaultTransition, href }: IButtonProps) => {
+
+const LoginButton = ({ value, defaultTransition, href }: ILoginButtonProps) => {
   return (
     <Link
       className={`rounded-xl border-2 ${defaultTransition} px-3 py-2 text-[${defaultUseColor}] border-[${defaultUseColor}] flex flex-row items-center justify-center gap-1.5 font-semibold hover:border-[#FF385C] hover:bg-[#FF385C] hover:text-white`}
