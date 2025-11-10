2dd09cf82a5559c1a0679274834a0ae58f4ca5f4
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-10 18:09:56 -0300

    fix: fix a error involving variables interpolation in className, causing a error to show component

--- src/widgets/SearchBar.tsx
+++ src/widgets/SearchBar.tsx
@@ -7,12 +7,12 @@ const SearchBar = () => {
     <div className="container mx-auto flex w-full max-w-2xl rounded-full border border-gray-200 px-2.5 py-1.5 shadow-2xl shadow-gray-300">
       <input
         type="text"
-        className={`w-full px-2 outline-0 placeholder-gray-400`}
+        className={`w-full px-2 placeholder-gray-400 outline-0`}
         placeholder="Comece a sua busca"
         id=""
         style={{
           color: defaultUseColor,
-          fontWeight: 500
+          fontWeight: 500,
         }}
       />
       <SearchButton />
