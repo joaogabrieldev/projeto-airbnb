8958e1da334d1de3232d0ad67a1500af78f83d59
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-11 17:30:13 -0300

    feat: created AccommodationTestimonials widget

---
+++ src/widgets/AccommodationTestimonials.tsx
@@ -0,0 +1,9 @@
+const AccommodationTestimonials = () => {
+  return (
+    <div className="w-full py-4">
+      <h3 className="text-xl font-semibold">Acomodação Depoimentos</h3>
+    </div>
+  );
+};
+
+export default AccommodationTestimonials;
