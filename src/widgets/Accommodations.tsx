ea5b1fb420a6084276fd74447140e101a878252e
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-11 17:29:35 -0300

    chore: finished Accommodation Full Widget

--- src/widgets/Accommodations.tsx
+++ src/widgets/Accommodations.tsx
@@ -1,42 +1,35 @@
-import { Heart } from "lucide-react";
-import Image from "next/image";
-
 import { accommodations } from "@/assets/accommodations";
 
-const Accommodations = () => {
-  const fillHeartColor = "rgba(28, 25, 23, 0.6)";
-  const strokeHeartColor = "white";
+import AccommadationCard from "../components/AccommodationCard";
+import AcommodationDesc from "./../components/AccommodationDesc";
+import AccommodationTitle from "./../components/AccommodationTitle";
 
+const Accommodations = () => {
   return (
-    <section className="mt-14 grid grid-cols-1 gap-x-2 gap-y-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+    <section className="mt-14 grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
       {accommodations.map((item, index) => {
         return (
           <div key={index}>
-            <div className="flex flex-row items-center justify-around gap-14">
-              <span>Preferido dos Hóspedes</span>
-              <button>
-                <Heart
-                  className="cursor-pointer"
-                  style={{
-                    fill: fillHeartColor,
-                    stroke: strokeHeartColor,
-                  }}
-                />
-              </button>
-            </div>
-            <Image
-              className="aspect-square w-full rounded-xl object-cover"
-              src={item.photos[0].source}
-              alt={item.photos[0].description}
-              width={300}
-              height={300}
+            <AccommodationTitle
+              label="Preferído dos hóspedes"
+              hasBadge={item.hasBadge}
             />
-            <div className="flex justify-between">
-              <div>{}</div>
-              <div>Rating</div>
-            </div>
-            <div>Horario</div>
-            <div>Preço</div>
+            <AccommadationCard
+              key={index}
+              alt={item.photos[0].description}
+              src={item.photos[0].source}
+              width={300}
+              slug={item.slug}
+            />
+            <AcommodationDesc
+              date={item.date}
+              host={item.host}
+              location={{
+                description: item.location.description,
+              }}
+              price={item.price}
+              rating={item.rating}
+            />
           </div>
         );
       })}
