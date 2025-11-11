64660cceeac7e267f95ede6536befca217b31179
joaogabrieldev0 <joaogabriel2r.profissional@hotmail.com>
2025-11-11 17:19:10 -0300

    feat: created accommodation card, component to show accommodation main image

--- src/components/AccommodationCard.tsx
+++ src/components/AccommodationCard.tsx
@@ -1,45 +1,28 @@
-import { IAccomodation } from "@/assets/assets.types";
-import { Heart } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
 import { IAccommodationCardProps } from "./components.types";
 
 const AccommadationCard = ({
-  className, 
   src,
   alt,
   width,
-  height, 
-  date, 
-  hasBadge,
-  host,
-  id,
-  location,
-  number,
-  photos,
-  price,
-  rating,
   slug,
-  testimonials,
-  title}:IAccomodation & IAccommodationCardProps) => {
-  const fillHeartColor = "rgb(28, 25, 23. 0.6)";
-  const strokeHeartColor = "white";
+}: IAccommodationCardProps) => {
+  const height = width;
 
   return (
-      <div>
-        <div className="flex flex-row items-center justify-around gap-14">
-              <span>Preferido dos Hóspedes</span>
-              <button>
-                <Heart
-                  className="cursor-pointer"
-                  style={{
-                    fill: fillHeartColor,
-                    stroke: strokeHeartColor,
-                  }}
-                />
-              </button>
-            </div>
-            <Image src={""} alt={""} />
-      </div>
+    <div className="rounded-[11px] shadow-md select-none">
+      <Link href={slug}>
+        <Image
+          className="aspect-square w-full rounded-xl object-cover"
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+        />
+      </Link>
+    </div>
   );
 };
 
