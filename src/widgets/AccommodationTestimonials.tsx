7ffd1a33fc570727a1345065d0e2e6c5cf60b80f
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-11-12 16:50:25 -0300

    feat: add Title and Body in AccommodationTestimonials

--- src/widgets/AccommodationTestimonials.tsx
+++ src/widgets/AccommodationTestimonials.tsx
@@ -1,7 +1,28 @@
-const AccommodationTestimonials = () => {
+import { Star } from "lucide-react";
+import AccomTestimonialsTitle from "../components/AccomTestimonialsTitle";
+import AccomTestimonialsBody from './../components/AccomTestimonialsBody';
+
+interface IAccTestimonialsProps {
+  rating: number;
+  accommodationArray: object[];
+}
+
+const AccommodationTestimonials = ({
+  rating,
+  accommodationArray,
+}: IAccTestimonialsProps) => {
+  const avaliations =
+    accommodationArray.length > 2 ? "avaliações" : "avaliação";
+
   return (
     <div className="w-full py-4">
-      <h3 className="text-xl font-semibold">Acomodação Depoimentos</h3>
+      <AccomTestimonialsTitle
+        title="Depoimentos"
+        rating={rating}
+        accommodationArray={accommodationArray}
+        avaliations={avaliations}
+      />
+      <AccomTestimonialsBody name={""} image={""} comment={""} rating={0} customerTime={0} createdAt={0} stayedAt={0} />
     </div>
   );
 };
