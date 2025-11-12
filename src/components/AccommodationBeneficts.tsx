98d3c8ac857f5e4a84f308f3c9784b008cb5b0e1
João G. R. Rocha <joaogabriel2r.profissional@hotmail.com>
2025-11-12 01:30:38 -0300

    chore: continue accommodationBeneficts component

--- src/components/AccommodationBeneficts.tsx
+++ src/components/AccommodationBeneficts.tsx
@@ -13,9 +13,9 @@ import {
 
 import Benefict from "./Benefict";
 
-interface IAccBenefictsProps {
-  label: string;
-}
+// interface IAccBenefictsProps {
+//   label: string;
+// }
 
 const AccommodationBeneficts = () => {
   return (
@@ -31,6 +31,38 @@ const AccommodationBeneficts = () => {
       <Benefict>
         <IconDesk />
         <span>Espaço de trabalho exclusivo</span>
+      </Benefict>
+      <Benefict>
+          <IconPool />
+          <span>Piscina privativa</span>
+      </Benefict>
+      <Benefict>
+        <IconBrandCarbon/>
+        <span>Alarme de segurança p/ gás</span>
+      </Benefict>
+      <Benefict>
+        <IconSailboat/>
+        <span>Vista para águas</span>
+      </Benefict>
+      <Benefict>
+        <IconWifi/>
+        <span>Wi-Fi rápido (83 Mbps)</span>
+      </Benefict>
+      <Benefict>
+        <IconParking/>
+        <span>Estacionamento incluído</span>
+      </Benefict>
+      <Benefict>
+        <IconParking/>
+        <span>Estacionamento incluído</span>
+      </Benefict>
+      <Benefict>
+        <IconDog/>
+        <span>Permitindo animais</span>
+      </Benefict>
+      <Benefict>
+        <IconAlarmSmoke/>
+        <span>Detector de fumaça</span>
       </Benefict>
     </ul>
   );
--- src/components/Benefict.tsx
+++ src/components/Benefict.tsx
@@ -3,7 +3,7 @@ interface IBenefictProps {
 }
 
 const Benefict = ({ children }: IBenefictProps) => {
-  return <li className="flex flex-row gap-1">{children}</li>;
+  return <li className="flex flex-row gap-1 mt-4">{children}</li>;
 };
 
 export default Benefict;
